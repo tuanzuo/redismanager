1d23ed7f25fd878b6885ed724a772d8e58d9086e
tt@dd.com <123456>
2019-05-30 22:47:17 +0800

    redis

---
+++ src/test/java/com/tz/redismanager/RedismanagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tz.redismanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedismanagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
