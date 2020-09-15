1a97aeb9fff8a86be56941a1ad1f803daefe9dc4
891615064@qq.com <123456>
2020-09-15 23:54:02 +0800

    Optimize logout code

--- src/main/java/com/tz/redismanager/controller/AuthController.java
+++ src/main/java/com/tz/redismanager/controller/AuthController.java
@@ -1,16 +1,14 @@
 package com.tz.redismanager.controller;
 
-import com.tz.redismanager.constant.ConstInterface;
 import com.tz.redismanager.domain.ApiResult;
 import com.tz.redismanager.domain.vo.LoginVO;
 import com.tz.redismanager.service.IAuthService;
+import com.tz.redismanager.token.TokenAuth;
+import com.tz.redismanager.token.TokenContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * 权限controller
@@ -31,9 +29,9 @@ public class AuthController {
     }
 
     @RequestMapping("logout")
-    public ApiResult<?> logout(HttpServletRequest request, HttpServletResponse response) {
-        String token = request.getHeader(ConstInterface.Auth.AUTHORIZATION);
-        return authService.logout(token);
+    @TokenAuth
+    public ApiResult<?> logout(TokenContext tokenContext) {
+        return authService.logout(tokenContext.getToken());
     }
 
 }
