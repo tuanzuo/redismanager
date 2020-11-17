96aa2ffcf5c16d7667c7cec427c2970323b0bcb5
tuanzuo <tuanzuo@tz.com>
2020-11-17 23:35:04 +0800

    update pageui

--- redismanager-web/src/main/resources/static/service-worker.js
+++ redismanager-web/src/main/resources/static/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.f6fe1fee227a6a2611694184fa8ef5a3.js", "/workbox-v3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.7c9fcf40f84522ab530306be1348a0b6.js", "/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v3.6.3"});
 /* globals workbox */
 /* eslint-disable no-restricted-globals */
