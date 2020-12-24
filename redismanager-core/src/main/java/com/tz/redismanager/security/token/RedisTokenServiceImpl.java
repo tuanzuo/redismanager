dd10d6c7744740bf5d3b7e91c52a476d837fe04e
tuanzuo <tuanzuo@tz.com>
2020-12-24 23:13:57 +0800

    update token

--- redismanager-core/src/main/java/com/tz/redismanager/security/token/RedisTokenServiceImpl.java
+++ redismanager-core/src/main/java/com/tz/redismanager/security/token/RedisTokenServiceImpl.java
@@ -2,13 +2,13 @@ package com.tz.redismanager.security.token;
 
 import com.tz.redismanager.constant.ConstInterface;
 import com.tz.redismanager.dao.domain.po.UserPO;
-import com.tz.redismanager.enm.ResultCode;
-import com.tz.redismanager.exception.RmException;
 import com.tz.redismanager.security.domain.AuthContext;
 import com.tz.redismanager.security.domain.AuthContextToRedis;
 import com.tz.redismanager.service.IAuthCacheService;
 import com.tz.redismanager.service.ICipherService;
 import com.tz.redismanager.token.config.TokenProperties;
+import com.tz.redismanager.token.domain.ResultCode;
+import com.tz.redismanager.token.exception.TokenException;
 import com.tz.redismanager.token.service.ITokenService;
 import com.tz.redismanager.util.JsonUtils;
 import com.tz.redismanager.util.UUIDUtils;
@@ -18,10 +18,10 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 /**
- * <p></p>
+ * <p>Redis实现Token</p>
  *
- * @author admin
- * @version 1.0
+ * @author tuanzuo
+ * @version 1.5.0
  * @time 2020-12-23 21:43
  **/
 @Service
@@ -60,7 +60,7 @@ public class RedisTokenServiceImpl implements ITokenService {
         String userInfoKey = ConstInterface.CacheKey.USER_AUTH + token;
         String authContextCache = stringRedisTemplate.opsForValue().get(userInfoKey);
         if (StringUtils.isBlank(authContextCache)) {
-            throw new RmException(ResultCode.TOKEN_AUTH_EXPIRE);
+            throw new TokenException(ResultCode.TOKEN_AUTH_EXPIRE);
         }
         return authContextCache;
     }
