0ab79cb4ed5f64a35edde6e1433beeb91c483a29
tuanzuo <tuanzuo@tz.com>
2021-08-28 21:14:21 +0800

    update page

--- redismanager-web/src/main/resources/static/precache-manifest.425c4b9aaadde14b86bf9f24431e4c3f.js
+++ redismanager-web/src/main/resources/static/precache-manifest.425c4b9aaadde14b86bf9f24431e4c3f.js
@@ -8,11 +8,11 @@ self.__precacheManifest = [
     "url": "/vendors.async.js"
   },
   {
-    "revision": "f313698e3ed70a2b9981",
+    "revision": "df4f6a3d2f72931ae613",
     "url": "/layouts__BasicLayout.chunk.css"
   },
   {
-    "revision": "f313698e3ed70a2b9981",
+    "revision": "df4f6a3d2f72931ae613",
     "url": "/layouts__BasicLayout.async.js"
   },
   {
@@ -200,44 +200,44 @@ self.__precacheManifest = [
     "url": "/index.html"
   },
   {
-    "revision": "5423f638851bd441057fb363969ac6c9",
-    "url": "/favicon.png.redis"
+    "revision": "c23aa881b8d0e959f6c0aeb5ae7e4baa",
+    "url": "/icons\\icon-192x192.png"
   },
   {
     "revision": "347329d0982682b42d40c8162817a940",
     "url": "/icons\\icon-128x128.png"
   },
   {
-    "revision": "c23aa881b8d0e959f6c0aeb5ae7e4baa",
-    "url": "/icons\\icon-192x192.png"
-  },
-  {
     "revision": "c891db020b04fc5afaccff5439d843cd",
     "url": "/favicon.png"
-  },
-  {
-    "revision": "694c4300be571fdd4d8b6abbe14d2e3f",
-    "url": "/favicon.png.back"
   },
   {
     "revision": "5661ce8e2472d2727e8fd0390b22ba37",
     "url": "/icons\\icon-512x512.png"
   },
   {
+    "revision": "9a723c40a87f1e96e339b0af5e3e42bc",
+    "url": "/img\\sAuJeJzSKbUmHfBQRzmZ.svg"
+  },
+  {
     "revision": "356efdc3a7c62a58403fb14854c46c71",
     "url": "/img\\wAhyIChODzsoKIOBHcBk.svg"
   },
   {
-    "revision": "9a723c40a87f1e96e339b0af5e3e42bc",
-    "url": "/img\\sAuJeJzSKbUmHfBQRzmZ.svg"
+    "revision": "5423f638851bd441057fb363969ac6c9",
+    "url": "/favicon.png.redis"
+  },
+  {
+    "revision": "70af7a35733166735063f90072ee1150",
+    "url": "/img\\HsIsxMZiWKrNUavQUXqx.svg"
   },
   {
     "revision": "e147bdebf32ef9cb7ce137ed0ed5d3a6",
     "url": "/img\\BiazfanxmamNRoxxVxka.png"
   },
   {
-    "revision": "70af7a35733166735063f90072ee1150",
-    "url": "/img\\HsIsxMZiWKrNUavQUXqx.svg"
+    "revision": "694c4300be571fdd4d8b6abbe14d2e3f",
+    "url": "/favicon.png.back"
   },
   {
     "revision": "1b92f9a438bb106d7f5d9394df1c88ab",
