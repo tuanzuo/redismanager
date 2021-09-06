54d0680e3163b878bf375f6770c412eda345df8d
tuanzuo <tuanzuo@tz.com>
2021-09-06 22:49:52 +0800

    update sql

--- redismanager-web/src/main/resources/sql/redisadmin.sql
+++ redismanager-web/src/main/resources/sql/redisadmin.sql
@@ -72,7 +72,12 @@ CREATE TABLE `t_user` (
   UNIQUE KEY `uidx_name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='用户表';
 
-insert  into `t_user`(`id`,`name`,`pwd`,`status`,`note`,`creater`,`create_time`,`updater`,`update_time`,`if_del`) values (1,'superadmin','7e7e3165a2ab371ad00102bbf440d9c3',1,'有人梦我与前尘','superadmin','2020-10-07 12:17:07','superadmin','2020-10-07 12:17:07',0),(2,'admin','1c7b1e2c168f93cf17b4fe288f2b5fb7',1,'有人与我立黄昏','admin','2020-10-07 12:19:08','admin','2020-10-07 12:19:08',0),(3,'develop','53da58e3c24e14e4abb1a59ef8e9efaf',1,'有人与我把酒分','develop','2020-10-07 12:19:51','develop','2020-10-07 12:19:51',0),(4,'test','4204ae17473c367b30419ecc63410ea4',1,'有人与我立黄昏','test','2020-10-07 12:20:06','test','2020-10-07 12:20:06',0);
+insert  into `t_user`(`id`,`name`,`pwd`,`status`,`note`,`creater`,`create_time`,`updater`,`update_time`,`if_del`)
+values
+(1,'superadmin','7e7e3165a2ab371ad00102bbf440d9c3',1,'有人梦我与前尘','superadmin','2020-10-07 12:17:07','superadmin','2020-10-07 12:17:07',0),
+(2,'admin','1c7b1e2c168f93cf17b4fe288f2b5fb7',1,'有人与我立黄昏','admin','2020-10-07 12:19:08','admin','2020-10-07 12:19:08',0),
+(3,'develop','53da58e3c24e14e4abb1a59ef8e9efaf',1,'有人与我把酒分','develop','2020-10-07 12:19:51','develop','2020-10-07 12:19:51',0),
+(4,'test','4204ae17473c367b30419ecc63410ea4',1,'有人与我立黄昏','test','2020-10-07 12:20:06','test','2020-10-07 12:20:06',0);
 
 CREATE TABLE `t_user_role_relation` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
@@ -87,7 +92,21 @@ CREATE TABLE `t_user_role_relation` (
   KEY `idx_user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='用户角色关系表';
 
-insert  into `t_user_role_relation`(`id`,`user_id`,`role_id`,`creater`,`create_time`,`updater`,`update_time`,`if_del`) values (1,1,1,'superadmin','2020-10-07 12:17:08','superadmin','2020-10-07 12:17:08',0),(2,1,2,'superadmin','2020-10-07 12:17:08','superadmin','2020-10-07 12:17:08',0),(3,1,3,'superadmin','2020-10-07 12:17:08','superadmin','2020-10-07 12:17:08',0),(4,1,4,'superadmin','2020-10-07 12:17:08','superadmin','2020-10-07 12:17:08',0),(5,2,2,'admin','2020-10-07 12:19:08','admin','2020-10-07 12:19:08',0),(6,2,3,'admin','2020-10-07 12:19:08','admin','2020-10-07 12:19:08',0),(7,2,4,'admin','2020-10-07 12:19:08','admin','2020-10-07 12:19:08',0),(8,3,2,'develop','2020-10-07 12:19:51','develop','2020-10-07 12:19:51',0),(9,3,3,'develop','2020-10-07 12:19:51','develop','2020-10-07 12:19:51',0),(10,3,4,'develop','2020-10-07 12:19:51','develop','2020-10-07 12:19:51',0),(11,4,2,'test','2020-10-07 12:20:06','test','2020-10-07 12:20:06',0),(12,4,3,'test','2020-10-07 12:20:06','test','2020-10-07 12:20:06',0),(13,4,4,'test','2020-10-07 12:20:06','test','2020-10-07 12:20:06',0);
+insert  into `t_user_role_relation`(`id`,`user_id`,`role_id`,`creater`,`create_time`,`updater`,`update_time`,`if_del`)
+values
+(1,1,1,'superadmin','2020-10-07 12:17:08','superadmin','2020-10-07 12:17:08',0),
+(2,1,2,'superadmin','2020-10-07 12:17:08','superadmin','2020-10-07 12:17:08',0),
+(3,1,3,'superadmin','2020-10-07 12:17:08','superadmin','2020-10-07 12:17:08',0),
+(4,1,4,'superadmin','2020-10-07 12:17:08','superadmin','2020-10-07 12:17:08',0),
+(5,2,2,'admin','2020-10-07 12:19:08','admin','2020-10-07 12:19:08',0),
+(6,2,3,'admin','2020-10-07 12:19:08','admin','2020-10-07 12:19:08',0),
+(7,2,4,'admin','2020-10-07 12:19:08','admin','2020-10-07 12:19:08',0),
+(8,3,2,'develop','2020-10-07 12:19:51','develop','2020-10-07 12:19:51',0),
+(9,3,3,'develop','2020-10-07 12:19:51','develop','2020-10-07 12:19:51',0),
+(10,3,4,'develop','2020-10-07 12:19:51','develop','2020-10-07 12:19:51',0),
+(11,4,2,'test','2020-10-07 12:20:06','test','2020-10-07 12:20:06',0),
+(12,4,3,'test','2020-10-07 12:20:06','test','2020-10-07 12:20:06',0),
+(13,4,4,'test','2020-10-07 12:20:06','test','2020-10-07 12:20:06',0);
 
 CREATE TABLE `t_config` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
@@ -108,9 +127,11 @@ CREATE TABLE `t_config` (
   KEY `idx_key` (`config_key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='配置表';
 
-INSERT INTO `t_config` (`service_name`, `config_type`, `config_key`, `key_name`, `content`, `version`, `note`, `creater`, `create_time`, `updater`, `update_time`, `if_del`) VALUES ('redismanagerweb', 10, 'rm:analysis:rds:conf', 'redis连接配置分析页缓存', '{\"asyncRefresh\":false,\"key\":\"rm:analysis:rds:conf\",\"l1Cache\":{\"enable\":false,\"expireDuration\":2,\"expireStrategy\":\"EXPIRE_AFTER_ACCESS\",\"expireUnit\":\"HOURS\",\"initialCapacity\":100,\"maximumSize\":1000,\"recordStats\":true},\"l2Cache\":{\"enable\":false,\"expireDuration\":10,\"expireUnit\":\"MINUTES\"},\"name\":\"redis连接配置分析页缓存\"}', 10, NULL, 'sys', '2021-04-22 23:01:37', 'superadmin', '2021-08-07 23:05:43', 0);
-INSERT INTO `t_config` (`service_name`, `config_type`, `config_key`, `key_name`, `content`, `version`, `note`, `creater`, `create_time`, `updater`, `update_time`, `if_del`) VALUES ('redismanagerweb', 30, 'CONFIG_LIST_API', '查询配置列表请求限流', '{\"key\":\"CONFIG_LIST_API\",\"name\":\"查询配置列表请求限流\",\"permits\":1,\"qps\":200.0,\"timeout\":0,\"unit\":\"MICROSECONDS\"}', 12, NULL, 'superadmin', '2021-04-23 22:46:53', 'superadmin', '2021-08-07 22:52:10', 0);
-INSERT INTO `t_config` (`service_name`, `config_type`, `config_key`, `key_name`, `content`, `version`, `note`, `creater`, `create_time`, `updater`, `update_time`, `if_del`) VALUES ('redismanagerweb', 30, 'CAPTCHA_API', '验证码限流', '{\"key\":\"CAPTCHA_API\",\"name\":\"验证码限流\",\"permits\":1,\"qps\":100.0,\"timeout\":0,\"unit\":\"MICROSECONDS\"}', 20, NULL, 'superadmin', '2021-04-24 19:26:43', 'superadmin', '2021-08-07 22:53:52', 0);
+INSERT INTO `t_config` (`id`,`service_name`, `config_type`, `config_key`, `key_name`, `content`, `version`, `note`, `creater`, `create_time`, `updater`, `update_time`, `if_del`)
+VALUES
+(1,'redismanagerweb', 10, 'rm:analysis:rds:conf', 'redis连接配置分析页缓存', '{\"asyncRefresh\":false,\"key\":\"rm:analysis:rds:conf\",\"l1Cache\":{\"enable\":false,\"expireDuration\":2,\"expireStrategy\":\"EXPIRE_AFTER_ACCESS\",\"expireUnit\":\"HOURS\",\"initialCapacity\":100,\"maximumSize\":1000,\"recordStats\":true},\"l2Cache\":{\"enable\":false,\"expireDuration\":10,\"expireUnit\":\"MINUTES\"},\"name\":\"redis连接配置分析页缓存\"}', 10, NULL, 'sys', '2021-04-22 23:01:37', 'superadmin', '2021-08-07 23:05:43', 0),
+(2,'redismanagerweb', 30, 'CONFIG_LIST_API', '查询配置列表请求限流', '{\"key\":\"CONFIG_LIST_API\",\"name\":\"查询配置列表请求限流\",\"permits\":1,\"qps\":200.0,\"timeout\":0,\"unit\":\"MICROSECONDS\"}', 12, NULL, 'superadmin', '2021-04-23 22:46:53', 'superadmin', '2021-08-07 22:52:10', 0),
+(3,'redismanagerweb', 30, 'CAPTCHA_API', '验证码限流', '{\"key\":\"CAPTCHA_API\",\"name\":\"验证码限流\",\"permits\":1,\"qps\":100.0,\"timeout\":0,\"unit\":\"MICROSECONDS\"}', 20, NULL, 'superadmin', '2021-04-24 19:26:43', 'superadmin', '2021-08-07 22:53:52', 0);
 
 CREATE TABLE `worker_node` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'auto increment id',
