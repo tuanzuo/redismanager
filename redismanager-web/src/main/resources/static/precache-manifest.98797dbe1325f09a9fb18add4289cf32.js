6b07849c70d0e61f6ca5b7575769febc1ef1c1a0
tuanzuo <tuanzuo@tz.com>
2021-11-17 01:16:59 +0800

    update postman module：add share to interface

---
+++ redismanager-web/src/main/resources/static/precache-manifest.98797dbe1325f09a9fb18add4289cf32.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = [
+  {
+    "revision": "285ca0aa664f6a2145bc",
+    "url": "/vendors.chunk.css"
+  },
+  {
+    "revision": "285ca0aa664f6a2145bc",
+    "url": "/vendors.async.js"
+  },
+  {
+    "revision": "08b7790402ce8a6959ab",
+    "url": "/layouts__BasicLayout.chunk.css"
+  },
+  {
+    "revision": "08b7790402ce8a6959ab",
+    "url": "/layouts__BasicLayout.async.js"
+  },
+  {
+    "revision": "b08e2ff04384faacbe04",
+    "url": "/layouts__BlankLayout.async.js"
+  },
+  {
+    "revision": "ccc99acc18c628722d77",
+    "url": "/layouts__UserLayout.chunk.css"
+  },
+  {
+    "revision": "ccc99acc18c628722d77",
+    "url": "/layouts__UserLayout.async.js"
+  },
+  {
+    "revision": "3cca6ea0ad6774d0e98c",
+    "url": "/p__Account__Settings__models__geographic.js.async.js"
+  },
+  {
+    "revision": "f39821e748dc3f0f9e6d",
+    "url": "/p__Account__Settings__models__useropt.js.async.js"
+  },
+  {
+    "revision": "222e202c7958f5deeaae",
+    "url": "/p__ConfigManager__models__configmanager.js.async.js"
+  },
+  {
+    "revision": "b9fe329dec99335ed1f7",
+    "url": "/p__Dashboard__models__activities.js.async.js"
+  },
+  {
+    "revision": "e6b31f45c9aadeb8a72a",
+    "url": "/p__Dashboard__models__chart.js.async.js"
+  },
+  {
+    "revision": "7d8c10e32044a189a3f2",
+    "url": "/p__Dashboard__models__monitor.js.async.js"
+  },
+  {
+    "revision": "8bae5ed506ac2b6172a7",
+    "url": "/p__Postman__models__postman.js.async.js"
+  },
+  {
+    "revision": "3023d28890d12c37bac3",
+    "url": "/p__Redis__models__redisadmin.js.async.js"
+  },
+  {
+    "revision": "c742563e5f73c1aba893",
+    "url": "/p__RoleManager__models__rolemanager.js.async.js"
+  },
+  {
+    "revision": "c5f52b6c8a43ac2e4130",
+    "url": "/p__UserManager__models__usermanager.js.async.js"
+  },
+  {
+    "revision": "ca292304083dad7d3353",
+    "url": "/p__User__Login.chunk.css"
+  },
+  {
+    "revision": "ca292304083dad7d3353",
+    "url": "/p__User__Login.async.js"
+  },
+  {
+    "revision": "519fb30c87c91983dbb1",
+    "url": "/p__User__Register.chunk.css"
+  },
+  {
+    "revision": "519fb30c87c91983dbb1",
+    "url": "/p__User__Register.async.js"
+  },
+  {
+    "revision": "0be5e34d6cf4d293d134",
+    "url": "/p__User__RegisterResult.chunk.css"
+  },
+  {
+    "revision": "0be5e34d6cf4d293d134",
+    "url": "/p__User__RegisterResult.async.js"
+  },
+  {
+    "revision": "3e9c463f4be3962872c6",
+    "url": "/p__User__models__captcha.js.async.js"
+  },
+  {
+    "revision": "b215aa2e7bda2fd073fe",
+    "url": "/p__User__models__register.js.async.js"
+  },
+  {
+    "revision": "87b43db53ab23837e0cc",
+    "url": "/umi.css"
+  },
+  {
+    "revision": "87b43db53ab23837e0cc",
+    "url": "/umi.js"
+  },
+  {
+    "revision": "cf1c89dbf71cb86fa128",
+    "url": "/20.chunk.css"
+  },
+  {
+    "revision": "cf1c89dbf71cb86fa128",
+    "url": "/20.async.js"
+  },
+  {
+    "revision": "cd18bd14e5ddf908b0d7",
+    "url": "/21.chunk.css"
+  },
+  {
+    "revision": "cd18bd14e5ddf908b0d7",
+    "url": "/21.async.js"
+  },
+  {
+    "revision": "0bf415486b45f701637e",
+    "url": "/22.chunk.css"
+  },
+  {
+    "revision": "0bf415486b45f701637e",
+    "url": "/22.async.js"
+  },
+  {
+    "revision": "8edbd1dd7fc04cd7e635",
+    "url": "/23.chunk.css"
+  },
+  {
+    "revision": "8edbd1dd7fc04cd7e635",
+    "url": "/23.async.js"
+  },
+  {
+    "revision": "418f9435b037b704d72d",
+    "url": "/24.chunk.css"
+  },
+  {
+    "revision": "418f9435b037b704d72d",
+    "url": "/24.async.js"
+  },
+  {
+    "revision": "9c2d745e5db40e2b9048",
+    "url": "/25.chunk.css"
+  },
+  {
+    "revision": "9c2d745e5db40e2b9048",
+    "url": "/25.async.js"
+  },
+  {
+    "revision": "71551d051104ebe011ce",
+    "url": "/26.chunk.css"
+  },
+  {
+    "revision": "71551d051104ebe011ce",
+    "url": "/26.async.js"
+  },
+  {
+    "revision": "ff58bacb4d42e1546323",
+    "url": "/27.async.js"
+  },
+  {
+    "revision": "d4a6ceaf1b55665156e3",
+    "url": "/28.async.js"
+  },
+  {
+    "revision": "8d8ade38f596da910fde",
+    "url": "/29.async.js"
+  },
+  {
+    "revision": "34ec8cb00492106cbade",
+    "url": "/30.async.js"
+  },
+  {
+    "revision": "4c3c594e674087c1aa67",
+    "url": "/31.async.js"
+  },
+  {
+    "revision": "078c7aa878745028d774",
+    "url": "/32.async.js"
+  },
+  {
+    "revision": "3d216c8e1ee01da902a0",
+    "url": "/33.async.js"
+  },
+  {
+    "revision": "60ba9d617cfd2219159c",
+    "url": "/34.async.js"
+  },
+  {
+    "revision": "43075320c6b7fc899f90",
+    "url": "/35.async.js"
+  },
+  {
+    "revision": "bbfd3931c7ea447058c9d3b7ebc88ae0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "694c4300be571fdd4d8b6abbe14d2e3f",
+    "url": "/favicon.png.back"
+  },
+  {
+    "revision": "347329d0982682b42d40c8162817a940",
+    "url": "/icons\\icon-128x128.png"
+  },
+  {
+    "revision": "5423f638851bd441057fb363969ac6c9",
+    "url": "/favicon.png.redis"
+  },
+  {
+    "revision": "c23aa881b8d0e959f6c0aeb5ae7e4baa",
+    "url": "/icons\\icon-192x192.png"
+  },
+  {
+    "revision": "c891db020b04fc5afaccff5439d843cd",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "346e09471362f2907510a31812129cd2",
+    "url": "/img\\github.png"
+  },
+  {
+    "revision": "2dec628d04c18c54f0b80bc80ce70593",
+    "url": "/img\\wAhyIChODzsoKIOBHcBk.svg"
+  },
+  {
+    "revision": "e793fb2e87cce0934a94edcdff293e52",
+    "url": "/img\\sAuJeJzSKbUmHfBQRzmZ.svg"
+  },
+  {
+    "revision": "5661ce8e2472d2727e8fd0390b22ba37",
+    "url": "/icons\\icon-512x512.png"
+  },
+  {
+    "revision": "7b34fcaac1317b402f276942a1db2c13",
+    "url": "/img\\HsIsxMZiWKrNUavQUXqx.svg"
+  },
+  {
+    "revision": "e147bdebf32ef9cb7ce137ed0ed5d3a6",
+    "url": "/img\\BiazfanxmamNRoxxVxka.png"
+  },
+  {
+    "revision": "1b92f9a438bb106d7f5d9394df1c88ab",
+    "url": "/js\\data-set.min.js"
+  },
+  {
+    "revision": "5e81b496874b87362faed6e1b7111f5a",
+    "url": "/js\\BizCharts.min.js"
+  },
+  {
+    "revision": "2f55e818c93c0ceef4ac442f319a69a1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "9ef055bcf0c4b1f0ac5663173730b010",
+    "url": "/pwacompat.min.js"
+  }
+];
