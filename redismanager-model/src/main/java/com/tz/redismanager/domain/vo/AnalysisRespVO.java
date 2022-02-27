0b948121e2868df1373e9008ceb51bb54366d29e
tuanzuo <tuanzuo@tz.com>
2022-02-27 20:42:57 +0800

    Perfect notes

--- redismanager-model/src/main/java/com/tz/redismanager/domain/vo/AnalysisRespVO.java
+++ redismanager-model/src/main/java/com/tz/redismanager/domain/vo/AnalysisRespVO.java
@@ -49,37 +49,79 @@ public class AnalysisRespVO {
     @Getter
     @Setter
     public static class VisitData {
+        /**
+         * 总量
+         */
         private Long total = 0L;
+        /**
+         * 当年总量
+         */
         private Long currentYearTotal = 0L;
+        /**
+         * 当月总量
+         */
         private Long currentMonthTotal = 0L;
+        /**
+         * 当日总量
+         */
         private Long dayTotal = 0L;
+        /**
+         * 每年访问量集合
+         */
         private List<VisitDetailData> totalDatas = new ArrayList<>();
+        /**
+         * 当年每月访问量集合
+         */
         private List<VisitDetailData> currentDatas = new ArrayList<>();
+
         public void addTotalDatas(VisitDetailData data){
             totalDatas.add(data);
         }
+
         public void addCurrentDatas(VisitDetailData data){
             currentDatas.add(data);
         }
     }
+
     /**
      * 用户访问数据
      */
     @Getter
     @Setter
     public static class UserVisitData {
+        /**
+         * 当年总量
+         */
         private Long currentYearTotal = 0L;
+        /**
+         * 当月总量
+         */
         private Long currentMonthTotal = 0L;
+        /**
+         * 当日总量
+         */
         private Long dayTotal = 0L;
+        /**
+         * 每年访问量集合
+         */
         private List<VisitDetailData> totalDatas = new ArrayList<>();
+        /**
+         * 当年每月访问量集合
+         */
         private List<VisitDetailData> currentDatas = new ArrayList<>();
+        /**
+         * 用户请求量排名集合
+         */
         private List<VisitDetailData> rangeDatas = new ArrayList<>();
+
         public void addTotalDatas(VisitDetailData data){
             totalDatas.add(data);
         }
+
         public void addCurrentDatas(VisitDetailData data){
             currentDatas.add(data);
         }
+
         public void addRangeDatas(VisitDetailData data){
             rangeDatas.add(data);
         }
@@ -88,16 +130,27 @@ public class AnalysisRespVO {
     @Getter
     @Setter
     public static class RedisConfigVisitData {
+        /**
+         * Redis连接配置使用次数集合
+         */
         private List<VisitDetailData> currentDatas = new ArrayList<>();
+
         public void addCurrentDatas(VisitDetailData data){
             currentDatas.add(data);
         }
     }
+
     @Getter
     @Setter
     public static class VisitDetailData {
+        /**
+         * 横坐标
+         */
         private String x;
-        //Long/Double
+        /**
+         * 纵坐标
+         * Long/Double
+         */
         private Object y;
     }
 
@@ -107,17 +160,35 @@ public class AnalysisRespVO {
     @Getter
     @Setter
     public static class UserData {
+        /**
+         * 用户总数
+         */
         private Integer total = 0;
+        /**
+         * 当日新增用户总数
+         */
         private Integer dayTotal = 0;
+        /**
+         * 用户数每日增加总数集合
+         */
         private List<UserDayData> dayDatas = new ArrayList<>();
+
         private void addDayDatas(UserDayData dayData){
             dayDatas.add(dayData);
         }
     }
+
     @Getter
     @Setter
     public static class UserDayData {
+        /**
+         * 日期
+         * 格式：yyyy-MM-dd
+         */
         private String x;
+        /**
+         * 数量
+         */
         private Integer y;
     }
 
@@ -127,17 +198,35 @@ public class AnalysisRespVO {
     @Getter
     @Setter
     public static class RoleData {
+        /**
+         * 角色总数
+         */
         private Integer total = 0;
+        /**
+         * 当日新增角色总数
+         */
         private Integer dayTotal = 0;
+        /**
+         * 角色数每日增加总数集合
+         */
         private List<RoleDayData> dayDatas = new ArrayList<>();
+
         private void addDayDatas(RoleDayData dayData){
             dayDatas.add(dayData);
         }
     }
+
     @Getter
     @Setter
     public static class RoleDayData {
+        /**
+         * 日期
+         * 格式：yyyy-MM-dd
+         */
         private String x;
+        /**
+         * 数量
+         */
         private Integer y;
     }
 
@@ -147,17 +236,35 @@ public class AnalysisRespVO {
     @Getter
     @Setter
     public static class RedisConfigData {
+        /**
+         * Redis连接配置总数
+         */
         private Integer total = 0;
+        /**
+         * Redis连接配置当天增加总数
+         */
         private Integer dayTotal = 0;
+        /**
+         * Redis连接配置每日增加总数集合
+         */
         private List<RedisConfigDayData> dayDatas = new ArrayList<>();
+
         private void addDayDatas(RedisConfigDayData dayData){
             dayDatas.add(dayData);
         }
     }
+
     @Getter
     @Setter
     public static class RedisConfigDayData {
+        /**
+         * 日期
+         * 格式：yyyy-MM-dd
+         */
         private String x;
+        /**
+         * 数量
+         */
         private Integer y;
     }
 }
